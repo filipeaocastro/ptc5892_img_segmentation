68b58d47c296dff9c570497d5c904d3056e859ae
filipeaocastro <filipeaocastro@gmail.com>
2023-07-30 15:06:24 -0300

    saving cropped images

--- create_df.py
+++ create_df.py
@@ -28,30 +28,6 @@ def create_dataframe(data_dir):
                 images_list.append(img[:, :, j])
                 masks_list.append(gt[:, :, j])
 
-
-    #     pass
-    
-    # patient_folders = [folder for folder in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, folder))]
-    # for patient_folder in patient_folders:
-    #     image_files = [file for file in os.listdir(os.path.join(data_dir, patient_folder)) if file.startswith("image")]
-    #     mask_files = [file for file in os.listdir(os.path.join(data_dir, patient_folder)) if file.startswith("mask")]
-        
-    #     image_indices = np.arange(len(image_files))
-    #     np.random.shuffle(image_indices)
-        
-    #     for i in image_indices:
-    #         image_path = os.path.join(data_dir, patient_folder, image_files[i])
-    #         mask_path = os.path.join(data_dir, patient_folder, mask_files[i])
-            
-    #         image = np.load(image_path)  # Assuming the file contains shape (x, y, 10)
-    #         mask = np.load(mask_path)    # Assuming the file contains shape (x, y, 10)
-            
-    #         for j in range(image.shape[2]):
-    #             patient_ids.append(patient_folder)
-    #             images_list.append(image[:, :, j])
-    #             masks_list.append(mask[:, :, j])
-    
-    # Create the pandas DataFrame
     data = {
         'patient_id': patient_ids,
         'image': images_list,
@@ -61,12 +37,53 @@ def create_dataframe(data_dir):
     
     return df
 
-# Example usage
-data_dir = "data"  # Replace with the path to the directory containing patient folders
-df = create_dataframe(data_dir)
-
-with open('imgs.pkl', 'wb') as f:
-    pickle.dump(df, f)
+def crop_center_image(image, crop_size):
+    height, width = image.shape[:2]
+    start_y = max(0, height // 2 - crop_size // 2)
+    end_y = min(height, height // 2 + crop_size // 2)
+    start_x = max(0, width // 2 - crop_size // 2)
+    end_x = min(width, width // 2 + crop_size // 2)
     
-# Save the DataFrame to a CSV file
-# df.to_csv("paired_images_masks.csv", index=True)
+    cropped_image = image[start_y:end_y, start_x:end_x]
+    return cropped_image
+
+def crop_center_images_in_dataframe(df, crop_size=168):
+    cropped_images = []
+    cropped_masks = []
+    
+    for index, row in df.iterrows():
+        image = row['image']
+        mask = row['mask']
+        
+        cropped_image = crop_center_image(image, crop_size)
+        cropped_mask = crop_center_image(mask, crop_size)
+        
+        cropped_images.append(cropped_image)
+        cropped_masks.append(cropped_mask)
+
+    for i, (image, mask) in enumerate(zip(cropped_images, cropped_masks)):
+        df['image'][i] = image
+        df['mask'][i] = mask
+
+    # return df
+
+
+crop = True
+
+if not crop:
+    data_dir = "data" 
+    df = create_dataframe(data_dir)
+
+    with open('imgs.pkl', 'wb') as f:
+        pickle.dump(df, f)
+else:
+    with open('imgs.pkl', 'rb') as f:
+        df = pickle.load(f)
+    crop_center_images_in_dataframe(df, crop_size=168)
+    with open('imgs_cropped.pkl', 'wb') as fw:
+        pickle.dump(df, fw)
+    
+
+
+    
+
