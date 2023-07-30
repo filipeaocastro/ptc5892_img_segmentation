4786140696eee46f94bcf6e40776b1f920b044ae
filipeaocastro <filipeaocastro@gmail.com>
2023-07-30 15:06:43 -0300

    add helper to iterate over data

--- classification_test.py
+++ classification_test.py
@@ -3,9 +3,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
+from tensorflow import keras
+from tensorflow.keras.utils import load_img
 
-with open('imgs.pkl', 'rb') as f:
+with open('imgs_cropped.pkl', 'rb') as f:
     df = pickle.load(f)
 
-pass
+class imgRetriever(keras.utils.Sequence):
+    """Helper to iterate over the data"""
 
+    def __init__(self, batch_size, img_size, imgs_df):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.imgs_df = imgs_df
+
+    def __len__(self):
+        return len(self.imgs_df['image']) // self.batch_size
+
+    def __getitem__(self, idx):
+        """Returns tuple (input, target) correspond to batch #idx."""
+        i = idx * self.batch_size
+        # batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        # batch_target_img_paths = self.target_img_paths[i : i + self.batch_size]
+        x = np.zeros((self.batch_size,) + self.img_size, dtype="float32")
+        for j, img in enumerate(self.imgs_df['image'][i : i + self.batch_size]):
+            x[j] = img
+        y = np.zeros((self.batch_size,) + self.img_size, dtype="float32")
+        for j, img in enumerate(self.imgs_df['mask'][i : i + self.batch_size]):
+            y[j] = img
+
+        return x, y
+
+pass
